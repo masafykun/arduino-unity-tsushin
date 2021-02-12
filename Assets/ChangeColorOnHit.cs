f33bce1aa05292ea32bde1ec2e1f93685ec4ae10
masafy <mail@masafy.org>
2021-02-12 18:37:05 +0900

    arduinoで手を振動させる
    arduinoで手を振動させたりする挙動の作成１

---
+++ Assets/ChangeColorOnHit.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ChangeColorOnHit : MonoBehaviour {
+
+	GameObject Serial;
+	SerialReadLeft script;
+
+	void Start () {
+		Serial = GameObject.Find ("SerialCom");
+		script = Serial.GetComponent<SerialReadLeft>();
+	}
+
+	public void LED_ON2(){
+		byte[] byte1 = new byte[1];
+		byte1 [0] = 66; 
+		script.Port2.Write (byte1, 0, 1);
+	}
+
+	public void LED_OFF2(){
+		byte[] byte1 = new byte[1];
+		byte1 [0] = 11;
+		script.Port2.Write (byte1, 0, 1);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		LED_ON2 ();
+	}
+
+	void OnCollisionExit(Collision collision){
+		LED_OFF2 ();
+	}
+}
---
+++ Assets/oyayubi.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oyayubi : MonoBehaviour {
+
+	GameObject Serial;
+	SerialReadLeft script;
+
+	void Start () {
+		Serial = GameObject.Find ("SerialCom");
+		script = Serial.GetComponent<SerialReadLeft>();
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		byte[] byte1 = new byte[1];
+		byte1 [0] = 101; 
+		script.Port2.Write (byte1, 0, 1);
+	}
+
+	void OnCollisionExit(Collision collision){
+		byte[] byte1 = new byte[1];
+		byte1 [0] = 001; 
+		script.Port2.Write (byte1, 0, 1);
+	}
+
+}
